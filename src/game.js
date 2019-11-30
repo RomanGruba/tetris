95b1843dadee3c737737ca093f53ecc7d12266d5
Roman Gruba <roman.gruba@gmail.com>
2019-11-30 23:47:33 +0200

    isPieceOutOFBounds

--- src/game.js
+++ src/game.js
@@ -22,10 +22,43 @@ export default class Game {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
+
+  activePiece = {
+    x: 0,
+    y: 0,
+    blocks: [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]
+  };
+
+  movePieceLeft() {
+    this.activePiece.x -= 1;
+    if (this.isPieceOutOFBounds()) {
+      this.activePiece.x += 1;
+    }
+  }
+
+  movePieceRight() {
+    this.activePiece.x += 1;
+    if (this.isPieceOutOFBounds()) {
+      this.activePiece.x -= 1;
+    }
+  }
+
+  movePieceDown() {
+    this.activePiece.y += 1;
+    if (this.isPieceOutOFBounds()) {
+      this.activePiece.y -= 1;
+    }
+  }
+
+  isPieceOutOFBounds() {
+    const playfield = this.playfield;
+    const { x, y } = this.activePiece;
+    return playfield[y] === undefined || playfield[y][x] === undefined;
+  }
 }
